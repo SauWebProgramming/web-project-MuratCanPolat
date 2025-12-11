518758143b32d606ecd08d2765de1c634c5884e8
MuratCanPolat <muratcanpolat2007@gmail.com>
2025-12-11 18:03:31 +0300

    Service worker path'leri düzeltildi

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'agar-clone-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/js/Game.js',
-  '/js/Player.js',
-  '/js/Bot.js',
-  '/js/Food.js',
+  './',
+  './index.html',
+  './style.css',
+  './js/Game.js',
+  './js/Player.js',
+  './js/Bot.js',
+  './js/Food.js',
   'https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/dist/confetti.browser.min.js'
 ];
 
